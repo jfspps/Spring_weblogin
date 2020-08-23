5886582abd7680b86be2833e8db47a49c87970ba
jfspps <jfsapps@gmail.com>
2020-08-23 21:52:03 +0100

    first commit

---
+++ src/main/java/com/springsecurity/weblogin/WebLoginApplication.java
@@ -0,0 +1,11 @@
+package com.springsecurity.weblogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebLoginApplication.class, args);
+    }
+}
